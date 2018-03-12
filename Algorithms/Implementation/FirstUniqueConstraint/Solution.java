de133b384f15dbafdf3f11d310cdcfe2f2849e30
jovelli <romulo8000@gmail.com>
2018-03-11 22:53:27 -0300

    FirstUniqueString algorithm

---
+++ Algorithms/Implementation/FirstUniqueConstraint/Solution.java
@@ -0,0 +1,58 @@
+
+import java.util.*;
+
+public class Solution {
+
+	private static String[] values = {"Rua", "Casa", "Casa", "Casa", "Casa", "Casa", "Rua", "Rua", "Rua", "Rua"}; 
+	private static List<String> lista = new ArrayList<String>();
+
+	static {
+		for (int i = 0; i < 10000000; i++)
+		{
+			if (i == 100000) {
+				lista.add("UNICA_PRIMEIRA");
+			}
+
+			if (i == 2400000) {
+				lista.add("UNICA_SEGUNDA");
+			}
+
+			if (i == 3500000) {
+				lista.add("UNICA_TERCEIRA");
+			}
+
+			lista.add(values[(int) (Math.random() * 10)]);
+		}		
+
+		System.out.println("End initialize");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Started");
+		System.out.println(returnFirstNonDuplicated());
+	}
+
+	private static String returnFirstNonDuplicated() {
+		String result = "";
+
+		for (int i = 0; i < lista.size(); i++) {
+			if (isDuplicated(lista.get(i), i)) { 
+				continue;
+			} else {
+			 	result = lista.get(i); 
+			 	break;
+			}
+		}
+
+		return result;
+	}
+
+	private static boolean isDuplicated(String string, int index) {
+		for (int j = 0; j < lista.size(); j++ ) {
+			if (index != j && string.equals(lista.get(j))) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
