e860935b69d7fc1b21dfcd98083d323b919910f1
jovelli <romulo8000@gmail.com>
2018-03-15 16:11:20 -0300

    Birthday chocolate using sliding window

---
+++ Algorithms/Implementation/BirthdayChocolate/Solution.java
@@ -0,0 +1,39 @@
+// Author: Romulo Jovelli
+// HackerHank: https://www.hackerrank.com/challenges/the-birthday-bar/forum/problem
+
+import java.util.*;
+
+public class Solution {
+
+    private static int solve(int[] s, int d, int m){
+        int result = 0, acc = 0;
+
+        for (int i = 0; i < s.length; i++) {
+            acc += s[i];
+
+            if (i >= m) {
+                acc -= s[i - m];
+            }
+
+            if (acc == d) {
+                result++;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] s = new int[n];
+
+        for(int s_i=0; s_i < n; s_i++){
+            s[s_i] = in.nextInt();
+        }
+        int d = in.nextInt();
+        int m = in.nextInt();
+
+        System.out.println(solve(s, d, m));
+    }
+}
